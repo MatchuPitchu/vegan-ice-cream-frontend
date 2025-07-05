b8386978feb75bdc73757ca64a230fe89b25446e
MatchuPitchu <michael.flohr@posteo.de>
2025-07-05 21:28:04 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'frontend-eis-mit-stil',
+  webDir: 'dist'
+};
+
+export default config;
